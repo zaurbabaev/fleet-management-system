0329faa1894f212c37d769c7aa875c05eaad2aa1
Zaur <zaurik87@gmail.com>
2024-10-22 16:46:04 +0400

    first commit

---
+++ src/main/java/com/kindsonthegenius/fleetmsv/FleetmsvApplication.java
@@ -0,0 +1,13 @@
+package com.kindsonthegenius.fleetmsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetmsvApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(FleetmsvApplication.class, args);
+    }
+}
